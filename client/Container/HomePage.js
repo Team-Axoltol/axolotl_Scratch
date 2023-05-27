cf81db1ff730a7738a9fbd7e490b8085e95de22d
staceyjhlee <staceyjlee18@gmail.com>
2023-05-27 09:25:50 -0400

    FE additions

---
+++ client/Container/HomePage.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Home = () => {
+    //input nav bar
+    //input comment and post feature
+    //stretch chat
+    //filter
+
+  return <div>Home</div>;
+};
+
+export default Home;
