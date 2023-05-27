b948834b91f2986f54255bb6f8b93ca548d0aa48
jamie <jmezhang@gmail.com>
2023-05-27 12:35:34 -0500

    feat: added controllers/routers for getting/creating posts on homepage

--- server/server.js
+++ server/server.js
@@ -4,12 +4,32 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const path = require("path");
 const { disconnect } = require("process");
+const homepageRouter = require('./routers/router')
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+
+
+
+
+
+app.use('/homepage', homepageRouter)
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, {err: err});
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 
 server.listen(8080, () => {
   console.log("Starting at", 8080);
